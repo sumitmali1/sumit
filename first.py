a82046917862cf3a39f5839749c51cb7afa3550c
sumitmali <sumitmali180@gmial.com>
2023-09-21 10:27:29 +0530

    new file:   first.py

---
+++ first.py
@@ -0,0 +1,6 @@
+a=int (input("enter your number"))
+b=int (input("enter your number"))
+print("sum is ",(a+b))
+print ("subtraction is ",(a-b))
+print("Multiplication is",(a*b))
+print("Division is ",(a/b))
